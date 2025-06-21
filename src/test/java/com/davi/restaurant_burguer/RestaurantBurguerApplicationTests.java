947a305c2f834ac04f25f05420e0d4d588d2d293
DaviGabriel1 <davig41441503@gmail.com>
2025-06-21 14:59:13 -0300

    feat: mock do StorageService para inicialização correta dos testes

--- src/test/java/com/davi/restaurant_burguer/RestaurantBurguerApplicationTests.java
+++ src/test/java/com/davi/restaurant_burguer/RestaurantBurguerApplicationTests.java
@@ -1,12 +1,17 @@
 package com.davi.restaurant_burguer;
 
+import com.davi.restaurant_burguer.interfaces.IStorageServiceAdapter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class RestaurantBurguerApplicationTests {
+
+	@MockitoBean
+	private IStorageServiceAdapter storageServiceAdapter;
 
 	@Test
 	void contextLoads() {
