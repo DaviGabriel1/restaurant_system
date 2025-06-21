a31ce79ccdd54e45d94203c6857f80ed361366c7
DaviGabriel1 <davig41441503@gmail.com>
2025-06-21 14:36:12 -0300

    feat: perfil de testes no applicationTests

--- src/test/java/com/davi/restaurant_burguer/RestaurantBurguerApplicationTests.java
+++ src/test/java/com/davi/restaurant_burguer/RestaurantBurguerApplicationTests.java
@@ -2,8 +2,10 @@ package com.davi.restaurant_burguer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestaurantBurguerApplicationTests {
 
 	@Test
